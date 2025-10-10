5e2c5564ffe7a904218ce2607808d64288fb3f7e
RagaTV <203480660+RagaTV@users.noreply.github.com>
2025-10-10 02:47:03 -0600

    Funcionalidad PlayerController y animaciones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveInput = new Vector3(0f, 0f, 0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        transform.position+=moveInput*speed*Time.deltaTime;
+
+        if(moveInput != Vector3.zero){
+            anim.SetBool("isMoving", true);
+        } else {
+            anim.SetBool("isMoving", false);
+        }
+
+        Vector3 currentScale = transform.localScale;
+
+        if (moveInput.x > 0)
+        {
+            currentScale.x = Mathf.Abs(currentScale.x); 
+        }
+        else if (moveInput.x < 0)
+        {
+            currentScale.x = -Mathf.Abs(currentScale.x);
+        }
+        transform.localScale = currentScale;
+
     }
 }
