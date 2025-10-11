b4b6df7921823ca4b40d59850e49bc5c235f6cf1
RagaTV <203480660+RagaTV@users.noreply.github.com>
2025-10-10 23:44:44 -0600

    Se modifico el PlayerController
    Se agrego la funcionalidad de Dash o Roll, y se agrego un txt con la explicacion del funcionamiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,44 +1,83 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float dashDistance = 3f;       
+    public float dashDuration = 0.2f;     
+    public float dashCooldown = 0.8f;
     public Animator anim;
 
-    // Start is called before the first frame update
+    private bool isDashing = false;
+    private bool canDash = true;
+    private Vector3 moveInput;
+    private Vector3 lastDirection;
+
     void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 moveInput = new Vector3(0f, 0f, 0f);
+        if (isDashing) return;
+
+        // Movimiento normal
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
-        transform.position+=moveInput*speed*Time.deltaTime;
 
-        if(moveInput != Vector3.zero){
+        transform.position += moveInput * speed * Time.deltaTime;
+
+        if (moveInput != Vector3.zero)
+        {
             anim.SetBool("isMoving", true);
-        } else {
+            lastDirection = moveInput;
+        }
+        else
+        {
             anim.SetBool("isMoving", false);
         }
 
-        Vector3 currentScale = transform.localScale;
-
+        // Voltear sprite
+        Vector3 spriteOrientation = transform.localScale;
         if (moveInput.x > 0)
-        {
-            currentScale.x = Mathf.Abs(currentScale.x); 
-        }
+            spriteOrientation.x = Mathf.Abs(spriteOrientation.x);
         else if (moveInput.x < 0)
-        {
-            currentScale.x = -Mathf.Abs(currentScale.x);
-        }
-        transform.localScale = currentScale;
+            spriteOrientation.x = -Mathf.Abs(spriteOrientation.x);
+        transform.localScale = spriteOrientation;
 
+        // Dash con Space
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        anim.SetTrigger("roll");
+
+        Vector3 dashDirection = (moveInput == Vector3.zero) ? lastDirection : moveInput;
+        Vector3 startPos = transform.position;
+        Vector3 targetPos = startPos + dashDirection * dashDistance;
+
+        float elapsed = 0f;
+        while (elapsed < dashDuration)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, elapsed / dashDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
