6f6560873b8315a16a50605e946be397d0150e59
vanesaRojas04 <202580798+vanesaRojas04@users.noreply.github.com>
2025-10-22 03:24:00 -0600

    Modificaciones de la escena Menu
    Cambié fondo y botones

--- Assets/Scripts/Menu/MenuSystem.cs
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class MenuSystem : MonoBehaviour
 {
     public void Jugar(){
-    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    	//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    	SceneManager.LoadScene("EscenaBryan");
     }
 
     public void Salir(){
