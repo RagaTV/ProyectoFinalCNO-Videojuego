b77a8fc0974eb5fbe4a58df474cfb71d977338f5
Seven707M <182245@upslp.edu.mx>
2025-11-25 01:21:00 -0600

    Actualizado cofres (Valor y spawn)

--- Assets/Scripts/SpawnChest.cs
+++ Assets/Scripts/SpawnChest.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnChest : MonoBehaviour
 {
     public GameObject[] chestPrefabs;
+    public float distancia = 1f;
+    private Vector3 spawnPosition; 
     
     void Start()
     {
@@ -18,16 +20,27 @@ public class SpawnChest : MonoBehaviour
 
     public void SpawnChestCamara()
     {
-        int chestIndex=Random.Range(0, chestPrefabs.Length);
-        //aparezca dentro de la camara y el jugador lo vea
+        int chestIndex = Random.Range(0, chestPrefabs.Length);
         Camera mainCamera = Camera.main;
-        Vector3 pos1=mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane)); 
+        bool aux = false;
+        //area de aparición
+        Vector3 pos1 = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane)); 
         Vector3 pos2 = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
-        float spawnX = Random.Range(pos1.x  , pos2.x );
-        float spawnY = Random.Range(pos1.y , pos2.y );
-        Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
-        Instantiate(chestPrefabs[chestIndex], spawnPosition,chestPrefabs[chestIndex].transform.rotation);
+        while (aux!=true)// encontrar una posicion libre
+        {
+            float spawnX = Random.Range(pos1.x, pos2.x);
+            float spawnY = Random.Range(pos1.y, pos2.y);
+            spawnPosition = new Vector3(spawnX, spawnY, 0f);
+            // si no hay colisión en 2D 
+            // OverlapCircle para un radio alrededor de la posicion
+            if(Physics2D.OverlapCircle(spawnPosition, distancia)==null)
+            {
+                Instantiate(chestPrefabs[chestIndex], spawnPosition, chestPrefabs[chestIndex].transform.rotation);
+                aux=true;
+            }
+        }
     }
+
     private IEnumerator SpawnC(float delay)
     {
         while (true)
